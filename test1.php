31e6d059e2fcd5af1e1f7f1253b8b54dc30a0afd
Traveler1572 <all_over_the_world2003@hotmail.com>
2023-02-15 23:56:13 +0900

    編集
    編集1

--- test1.php
+++ test1.php
@@ -1,5 +1,16 @@
-<?php
-    $a = 123;
-    $b = '鈴木';
-    echo "'私の番号は'$a'で、名前は'$b'です。";
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>掲示板テスト</title>
+</head>
+<body>
+    <form action="test1.php" method="post">
+        <input type="text" name="test_name">
+        <textarea name="contents" rows="8" cols="40">
+        </textarea>
+    </form>
+</body>
+</html>
