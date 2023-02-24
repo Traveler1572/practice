3ee9a28cadc86a2d4399031e2fd2bc79a973eb27
Traveler1572 <all_over_the_world2003@hotmail.com>
2023-02-25 01:08:26 +0900

    編集
    データベース接続関連追記

--- test1.php
+++ test1.php
@@ -1,9 +1,49 @@
+<?php
+/*
+// データベースへの接続情報
+$host = 'localhost'; // MySQLが稼動しているサーバのアドレス
+$dbname = 'testbbs1'; // データベース名
+$user = 'root';     // データベースにアクセスするユーザ名
+$pass = '';         // データベースにアクセスするユーザのパスワード
+
+// データベースに接続
+try {
+    $dbh = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+}
+
+// POSTされたデータをデータベースに格納する
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 投稿内容を取得
+    $test_name = htmlspecialchars(filter_input(INPUT_POST,'test_name'), ENT_QUOTES, 'UTF-8');
+    $test_mail = htmlspecialchars(filter_input(INPUT_POST,'test_mail'), ENT_QUOTES, 'UTF-8');
+    $test_title = htmlspecialchars(filter_input(INPUT_POST,'test_title'), ENT_QUOTES, 'UTF-8');
+    $test_toukou = htmlspecialchars(filter_input(INPUT_POST,'test_toukou'), ENT_QUOTES, 'UTF-8');
+
+    // データベースに格納する
+    $stmt = $dbh->prepare('INSERT INTO messages (test_name, test_mail, test_title, test_toukou) VALUES (:test_name, :test_mail, :test_title, :test_toukou)');
+    $stmt->bindParam(':test_name', $test_name, PDO::PARAM_STR);
+    $stmt->bindParam(':test_mail', $test_mail, PDO::PARAM_STR);
+    $stmt->bindParam(':test_title', $test_title, PDO::PARAM_STR);
+    $stmt->bindParam(':test_toukou', $test_toukou, PDO::PARAM_STR);
+    $stmt->execute();
+}
+
+// データベースから投稿内容を取得する
+$stmt = $dbh->prepare('SELECT * FROM messages ORDER BY created_at DESC');
+$stmt->execute();
+$messages = $stmt->fetchAll(PDO::FETCH_ASSOC); */
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>掲示板テスト</title>
 </head>
 <body>
@@ -30,6 +70,9 @@
     print('<p>タイトル:'.$test_title.'</p>');
     print('<p>本文:'.$test_toukou.'</p>');
     print('<hr>');
+
+    #投稿内容一覧
+    
     ?>
 </body>
 </html>
